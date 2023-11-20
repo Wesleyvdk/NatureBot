f3310f6dfee18473a0bccc843e0ebf0a98fa7416
Wesley Van Der Kraan <shinxisrl@outlook.com>
2023-11-20 15:20:19 +0100

    V2 updates

--- deploy-commands.js
+++ deploy-commands.js
@@ -11,41 +11,50 @@ const commandFolders = fs.readdirSync(foldersPath);
 const token = process.env.BOT_TOKEN;
 
 for (const folder of commandFolders) {
-    // Grab all the command files from the commands directory you created earlier
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs
-        .readdirSync(commandsPath)
-        .filter((file) => file.endsWith(".js"));
-    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        commands.push(command.data.toJSON());
+  // Grab all the command files from the commands directory you created earlier
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      commands.push(command.data.toJSON());
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+      );
     }
+  }
 }
 
-const rest = new REST({ version: "10" }).setToken(token);
+const rest = new REST().setToken(token);
 
 (async () => {
-    try {
-        console.log(
-            `Started refreshing ${commands.length} application (/) commands.`
-        );
-        // for (let i = 0; i < commands.length; i++) {
-        //   console.log(commands[i].name);
-        //   console.log(commands[i].description);
-        // }
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`
+    );
+    // for (let i = 0; i < commands.length; i++) {
+    //   console.log(commands[i].name);
+    //   console.log(commands[i].description);
+    // }
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(Routes.applicationCommands(process.env.CLIENTID), {
-            body: commands,
-        });
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationCommands(process.env.CLIENTID),
+      {
+        body: commands,
+      }
+    );
 
-        console.log(
-            `Successfully reloaded ${data.length} application (/) commands.`
-        );
-    } catch (error) {
-        // And of course, make sure you catch and log any errors!
-        console.error(error);
-    }
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
 })();

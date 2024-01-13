940c7f2922d2ced7d7eb0b608c781fbf169e1e26
wesleyvdk <shinxisrl@outlook.com>
2024-01-13 16:58:45 +0100

    update bump

--- bot.js
+++ bot.js
@@ -294,30 +294,28 @@ client.on("messageCreate", async (message) => {
   try {
     if (embed) {
       try {
-        if (message.author.id === "938036238101921844") {
-          if (embed.description.includes(keyword)) {
-            message.channel.send(
-              `thank you for bumping! next bump is ready in 2 hours. to get the bump reminder role, use \`.addBump\`\n
+        if (embed.description.includes(keyword)) {
+          message.channel.send(
+            `thank you for bumping! next bump is ready in 2 hours. to get the bump reminder role, use \`.addBump\`\n
               if this role does not exist, the role will be created on first use of the .addBump command`
-            );
+          );
 
-            const delay = 2 * 60 * 60 * 1000;
-            // in milliseconds
-            if (role) {
-              const roleMention = role.toString();
-              setTimeout(() => {
-                // Send a message to the channel where the command was used
-                message.channel.send(`bump is ready ${roleMention}`);
-              }, delay);
-            } else {
-              setTimeout(() => {
-                // Send a message to the channel where the command was used
-                message.channel.send("bump is ready");
-              }, delay);
-            }
+          const delay = 2 * 60 * 60 * 1000;
+          // in milliseconds
+          if (role) {
+            const roleMention = role.toString();
+            setTimeout(() => {
+              // Send a message to the channel where the command was used
+              message.channel.send(`bump is ready ${roleMention}`);
+            }, delay);
           } else {
-            return;
+            setTimeout(() => {
+              // Send a message to the channel where the command was used
+              message.channel.send("bump is ready");
+            }, delay);
           }
+        } else {
+          return;
         }
       } catch (e) {
         console.log(`Error: ${e}`);

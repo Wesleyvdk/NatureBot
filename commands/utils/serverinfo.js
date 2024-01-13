978594530ef437265ad66f9a1955ff7c5b123670
wesleyvdk <shinxisrl@outlook.com>
2024-01-13 17:42:19 +0100

    updated commands

--- commands/utils/serverinfo.js
+++ commands/utils/serverinfo.js
@@ -37,7 +37,7 @@ module.exports = {
     function checkOnlineUsers(guild) {
       let onlineCount = 0;
       guild.members.cache.forEach((member) => {
-        if (member.user.presence.status === "online") onlineCount++;
+        if (member.presence.status === "online") onlineCount++;
       });
       return onlineCount;
     }

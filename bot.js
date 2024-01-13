6076514fa7b43f8845a71b6196145245d7c4470c
wesleyvdk <shinxisrl@outlook.com>
2024-01-13 16:48:35 +0100

    update bump reminder

--- bot.js
+++ bot.js
@@ -294,7 +294,7 @@ client.on("messageCreate", async (message) => {
   try {
     if (embed) {
       try {
-        if (message.author.id === "938036238101921844")
+        if (message.author.id === "938036238101921844") {
           if (embed.description.includes(keyword)) {
             message.channel.send(
               `thank you for bumping! next bump is ready in 2 hours. to get the bump reminder role, use \`.addBump\`\n
@@ -318,6 +318,7 @@ client.on("messageCreate", async (message) => {
           } else {
             return;
           }
+        }
       } catch (e) {
         console.log(`Error: ${e}`);
         console.log(`Date/Time: ${CurrentDate}`);

8392b7d488639db234cb4944a18b26acf4aebc4e
wesleyvdk <shinxisrl@outlook.com>
2024-01-22 20:32:04 +0100

    updated dice and coinflip

--- commands/games/dice.js
+++ commands/games/dice.js
@@ -27,12 +27,12 @@ module.exports = {
         .setName("eyes")
         .setDescription("set the amount of eyes")
         .addChoices(
-          { name: "1", value: "1" },
-          { name: "2", value: "2" },
-          { name: "3", value: "3" },
-          { name: "4", value: "4" },
-          { name: "5", value: "5" },
-          { name: "6", value: "6" }
+          { name: 1, value: 1 },
+          { name: 1, value: 2 },
+          { name: 1, value: 3 },
+          { name: 4, value: 4 },
+          { name: 5, value: 5 },
+          { name: 6, value: 6 }
         )
         .setRequired(true)
     ),

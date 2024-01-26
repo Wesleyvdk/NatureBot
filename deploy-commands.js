da1d075773345d1d053b33972b0769648c3dd533
wesleyvdk <shinxisrl@outlook.com>
2024-01-26 13:50:31 +0100

    removed guild specific commands (duplicate commands)

--- deploy-commands.js
+++ deploy-commands.js
@@ -38,18 +38,6 @@ const rest = new REST().setToken(token);
 (async () => {
   try {
     // for guild-based commands
-    await rest
-      .put(
-        Routes.applicationGuildCommands(
-          process.env.CLIENTID,
-          process.env.GUILDID
-        ),
-        {
-          body: [],
-        }
-      )
-      .then(() => console.log("Successfully deleted all guild commands."))
-      .catch(console.error);
 
     console.log(
       `Started refreshing ${commands.length} application (/) commands.`

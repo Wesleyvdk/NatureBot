8d16e746aa7bb80f29d47c72874f8a2bbb16d9af
Wesleyvdk <shinxisrl@outlook.com>
2024-04-22 15:43:50 +0200

    added await

--- commands/utils/getConfess.js
+++ commands/utils/getConfess.js
@@ -34,9 +34,9 @@ module.exports = {
       const message = interaction.options.getString("id");
       userid = interaction.user.id;
       username = interaction.user.username;
-      db = mongoclient.db("Aylani");
-      collection = db.collection("confessions");
-      const confession = collection.findOne({ id: id });
+      db = await mongoclient.db("Aylani");
+      collection = await db.collection("confessions");
+      const confession = await collection.findOne({ id: id });
       const user = await client.users.fetch(confession.user);
       interaction.editReply(
         `confession with id: ${id} was posted by ${user.username} (id: ${confession.user})`
